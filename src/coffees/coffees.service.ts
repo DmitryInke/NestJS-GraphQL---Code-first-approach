61b81ea1af565a3c9d3528b4498d35b2d3f41289
Dmitry Inke <angranity1998@gmail.com>
2024-01-30 00:18:28 +0200

    14/24

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCoffeeInput } from './dto/create-coffee.input';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Coffee } from './enitites/coffee.entity';
 
 @Injectable()
 export class CoffeesService {
+  constructor(
+    @InjectRepository(Coffee)
+    private readonly coffeesRepository: Repository<Coffee>,
+  ) {}
   async findAll() {
     return [];
   }
